b7a34545d6fb70a6ee09a7b7916f9afaf8212fc2
farhoud <farhoud.m7@gmail.com>
2019-05-11 22:11:15 +0430

    Project Extension Structure Initiated

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,33 +1,26 @@
 <template>
-    <v-btn
-            icon
-            color="#21dc78"
-            v-if="showSend"
-            @click="sendUserMessage"
-    >
-        <!--<img :src="sendUrl" alt="" width="16px" height="16px">-->
-    </v-btn>
+  <v-btn icon color="#21dc78">
+    <!--<img :src="sendUrl" alt="" width="16px" height="16px">-->
+  </v-btn>
+  <v-btn icon color="#21dc78">
+    <!--<img :src="sendUrl" alt="" width="16px" height="16px">-->
+  </v-btn>
 </template>
 
 <script>
-    global.browser = require('webextension-polyfill');
-    global.browser = require('@webcomponents/webcomponentsjs/webcomponents-bundle');
-    global.browser = require('@webcomponents/webcomponentsjs/custom-elements-es5-adapter');
-    import store from '../store';
+global.browser = require("webextension-polyfill");
+global.browser = require("@webcomponents/webcomponentsjs/webcomponents-bundle");
+global.browser = require("@webcomponents/webcomponentsjs/custom-elements-es5-adapter");
+import store from "../store";
 
-    export default {
-        store,
-        components: {},
-        props: [
-            'show',
-        ],
-        created() {
-
-        },
-    };
+export default {
+  store,
+  components: {},
+  props: ["show"],
+  created() {}
+};
 </script>
 
 <style>
-    @import '~vuetify/dist/vuetify.min.css';
-
+@import "~vuetify/dist/vuetify.min.css";
 </style>
--- src/app/main.js
+++ src/app/main.js
@@ -1,22 +1,23 @@
-global.browser = require('webextension-polyfill');
-global.browser = require('@webcomponents/webcomponentsjs/webcomponents-bundle');
-global.browser = require('@webcomponents/webcomponentsjs/custom-elements-es5-adapter');
+global.browser = require("webextension-polyfill");
+global.browser = require("@webcomponents/webcomponentsjs/webcomponents-bundle");
+global.browser = require("@webcomponents/webcomponentsjs/custom-elements-es5-adapter");
 
-
-import Vue from 'vue';
-import App from './App.vue?shadow';
-import wrap from '@vue/web-component-wrapper';
-import Vuetify from 'vuetify';
+import Vue from "vue";
+import App from "./App.vue?shadow";
+import wrap from "@vue/web-component-wrapper";
+import Vuetify from "vuetify";
 
 Vue.use(Vuetify);
 
 Vue.prototype.$browser = global.browser;
 window.Vue = Vue;
 const CustomElement = wrap(Vue, App);
-window.customElements.define('my-sanaz', CustomElement);
-let root = document.createElement('my-sanaz');
-root.setAttribute('style', `
-        display:'none';
+window.customElements.define("my-sanaz", CustomElement);
+let root = document.createElement("my-sanaz");
+root.setAttribute(
+  "style",
+  `
+        display:'block';
         background-color: #f5f5f6;
         position: fixed;
         top: 10%;
@@ -25,30 +26,31 @@ root.setAttribute('style', `
         max-width: 400px;
         padding: 0px;
         box-sizing: border-box;
-        z-index: 9999999999999999 ;`);
-root.setAttribute('id', 'sanaz-container');
+        z-index: 9999999999999999 ;`
+);
+root.setAttribute("id", "sanaz-container");
 
-root.setAttribute('show', 'true');
+root.setAttribute("show", "true");
 document.body.appendChild(root);
 
-window.addEventListener('message', function(event) {
-  // We only accept messages from ourselves
-  if (event.source != window)
-    return;
-
-  if (event.data.type && (event.data.type == 'TFXI_HANDSHAKEُ')) {
-    console.log('Sanaz received: ' + JSON.stringify(event.data));
-  }
-}, false);
-
-
-chrome.runtime.onMessage.addListener((msgObj, sender, sendResponse) => {
-  let sanazElement = document.getElementById('sanaz-container');
-  if (msgObj.hasOwnProperty('event') && msgObj.event === 'toggle') {
-    let showSanaz = sanazElement.style.display == 'block';
-    showSanaz = !showSanaz;
-    sanazElement.style.display = showSanaz ? 'block' : 'none';
-    sanazElement.show = showSanaz ? 'true' : 'none';
-  }
-  sendResponse({ farewell: showSanaz ? 'Hi' : 'Goodbye' });
-});
+// window.addEventListener('message', function(event) {
+//   // We only accept messages from ourselves
+//   if (event.source != window)
+//     return;
+//
+//   if (event.data.type && (event.data.type == 'TFXI_HANDSHAKEُ')) {
+//     console.log('Sanaz received: ' + JSON.stringify(event.data));
+//   }
+// }, false);
+//
+//
+// chrome.runtime.onMessage.addListener((msgObj, sender, sendResponse) => {
+//   let sanazElement = document.getElementById('sanaz-container');
+//   if (msgObj.hasOwnProperty('event') && msgObj.event === 'toggle') {
+//     let showSanaz = sanazElement.style.display == 'block';
+//     showSanaz = !showSanaz;
+//     sanazElement.style.display = showSanaz ? 'block' : 'none';
+//     sanazElement.show = showSanaz ? 'true' : 'none';
+//   }
+//   sendResponse({ farewell: showSanaz ? 'Hi' : 'Goodbye' });
+// });
